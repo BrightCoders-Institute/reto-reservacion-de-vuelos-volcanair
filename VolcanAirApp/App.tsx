f59d0f9a9ec3b06b10c320fbd47f6544f02d59e8
Antonio3veces3 <cramirez28@ucol.mx>
2023-06-29 18:27:04 -0600

    Resolve conflict
    Modify App.tsx to resolve conflict to merge to branch pre-develop
    
    Co-authored-by: FebernalGober <alexis.ferber18@hotmail.com>
    Co-authored-by: J3fte <santillanabdiel0@gmail.com>

--- VolcanAirApp/App.tsx
+++ VolcanAirApp/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import LoginScreen from './src/components/LoginScreen';
-import {
-  SafeAreaView, View
-} from 'react-native';
-import SignWithGoogle from './src/components/SignWithGoogle';
-import RegisterScreen from './src/components/RegisterScreen';
-function App(): JSX.Element {
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUpScreen from './src/Screens/SignUpScreen';
+import MyFlights from './src/Screens/MyFlights';
+import BookingScreen from './src/Screens/BookingScreen';
 
+const Stack= createNativeStackNavigator();
+function App(): JSX.Element {
   return (
-    <View>
-        <SafeAreaView >
-          <RegisterScreen />
-      </SafeAreaView>
-    </View>
-  
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='SignUp'>
+        <Stack.Screen name='SignUp' component={SignUpScreen} options={{title:"Signup"}}/>
+        <Stack.Screen name='MyFlights' component={MyFlights} options={{title:"My Flights"}}/>
+        <Stack.Screen name='Booking' component={BookingScreen} options={{title:"Boooking"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
