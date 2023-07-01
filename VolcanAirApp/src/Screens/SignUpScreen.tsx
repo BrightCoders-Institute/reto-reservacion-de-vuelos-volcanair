126af6ebfffafe72d47e000c7ca3eaf502d1cd2b
Antonio3veces3 <cramirez28@ucol.mx>
2023-06-30 19:51:29 -0600

    Create initial form to SignUpScreen
    Add inputs,labels and checkbox. Add styling to Screen and elements
    
    Co-authored-by: FebernalGober <alexis.ferber18@hotmail.com>
    Co-authored-by: J3fte <santillanabdiel0@gmail.com>

--- VolcanAirApp/src/Screens/SignUpScreen.tsx
+++ VolcanAirApp/src/Screens/SignUpScreen.tsx
@@ -1,13 +1,93 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import React, {useState} from 'react';
+import {Input} from 'react-native-elements';
+import CheckBox from '@react-native-community/checkbox';
 
+export default function SignUpScreen({navigation}): JSX.Element {
+  const [username, setUSername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [agreeTerms, setAgreeTems] = useState(false);
 
-export default function SignUpScreen({ navigation }): JSX.Element {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>SignUpScreen</Text>
-            <Button title='Go to My Flights' onPress={() => navigation.navigate('MyFlights')} />
-        </View>
-    )
+  return (
+    <SafeAreaView style={styles.savScreen}>
+      <Text style={styles.txtTitleScreen}>Sign Up</Text>
+      <View style={styles.vwFormContainer}>
+        <Text style={styles.txtLabelInput}>First Name</Text>
+        <Input
+          style={styles.input}
+          placeholder="Your name"
+          onChangeText={setUSername}
+        />
+        <Text style={styles.txtLabelInput}>Email*</Text>
+        <Input
+          style={styles.input}
+          placeholder="user@email.com"
+          onChangeText={setEmail}
+        />
+        <Text style={styles.txtLabelInput}>Password*</Text>
+        <Input
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={setPassword}
+          secureTextEntry={true}
+        />
+        <Text style={styles.txtInfoPassword}>
+          Use 8 or more characters with a mix of letters, numbers, and symbols
+        </Text>
+
+        <CheckBox
+          disabled={false}
+          value={agreeTerms}
+          onValueChange={() => setAgreeTems(!agreeTerms)}
+          tintColors={{true: "#5C72E5"}}
+        />
+      </View>
+
+      <Button
+        title="Go to My Flights"
+        onPress={() => navigation.navigate('MyFlights')}
+      />
+    </SafeAreaView>
+  );
 }
+//536CE6
+const styles = StyleSheet.create({
+  savScreen: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  txtTitleScreen: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#5C72E5',
+    paddingLeft: 10,
+  },
+  vwFormContainer: {
+    marginTop: 20,
+  },
+  txtLabelInput: {
+    fontSize: 18,
+    paddingLeft: 10,
+  },
+  input: {
+    marginTop: 6,
+    borderWidth: 1,
+    borderColor: '#474747',
+    width: '110%',
+  },
+  txtInfoPassword: {
+    marginTop: -18,
+    fontSize: 14,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
